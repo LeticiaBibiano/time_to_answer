5cc79dbd2013944c5b8b9699eef0408a2481c487
Leticia Bibiano <leticiabibianob@gmail.com>
2022-01-25 16:13:13 -0300

    Gitignore node_modules & tmp - answers view in progress

--- db/migrate/20220120133439_create_answers.rb
+++ db/migrate/20220120133439_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
     create_table :answers do |t|
       t.references :question, null: false, foreign_key: true
       t.text :description, null: false
-      t.boolean :correct, default: false
+      t.boolean :correct, default: false, null: false
 
       t.timestamps
     end
